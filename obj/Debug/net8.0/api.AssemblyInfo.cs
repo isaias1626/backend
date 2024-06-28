ffa51b0d7ab1b40a47efda3817adcd49e82a073f
¨isaias <isaiasrocha1626@gmail.com>
2024-06-28 09:23:21 -0300

    feat: api

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7baf5907e71389ce4ae15de50c1f08ee6217375c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a86faeea3e713da4d3a47bff7cea04bf258b419")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
